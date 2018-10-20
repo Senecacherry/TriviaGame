133dd9e9cc46ff487877046623844bcf2488e454
Senecacherry <42197371+Senecacherry@users.noreply.github.com>
2018-10-19 20:34:59 -0600

    More Java

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,7 +16,7 @@ $(document).on("click", "#start", function(){
     game.loadQuestion();
 });
 
-//next we enter the content that will be displayed when I call the function
+//next I entered the content that will be displayed when I call the function
 
 var questions = [{
     question: "What duo created Rick and Morty?",
@@ -31,7 +31,76 @@ var questions = [{
 }, {
     question: "What is Beth's job?",
     answers: ["Security Officer", "Heart Surgeon", "Horse Surgeon", "Model"],
-    correctAnswer: "Horse Surgeon"
-    image: "assets/images/nice.gif",
-},
+    correctAnswer: "Horse Surgeon",
+    image: "assets/images/nice.gif"
+}, {
+    question: "What is Rick's 'Universe Number?",
+    answers: ["C-142", "C-137", "C-139", "C-133"],
+    correctAnswer: "C-137",
+    image:"assets/images/shake.gif"
+}, {
+    question: "What movie does Rick and Morty draw inspiration from?",
+    answers: ["Alien", "Starship Troopers", "Back to The Future", "Willy Wonka and The Chocolate Factory"],
+    correctAnswer: "Back to The Future",
+    image:"assets/images/summer.gif"
+}, {
+    question: "What device does Rick use for interdimensional travel?",
+    answers: ["A wand", "Plumbas", "Portal Gun", "Ray gun"],
+    correctAnswer: "Portal Gun",
+    image: "assets/images/blink.gif"
+}, {
+    question: "What actress voices Summer?",
+    answers: ["Spencer Grammer", "Laura Bailey", "Amy Schumer", "Tara Strong"],
+    correctAnswer: "Spencer Grammer",
+    image: "assets/images/annoyed.gif"
+}, {
+    question: "Who got shot because Beth thought he was a parasite?",
+    answers: ["Mr. Meseeks", "Pencilvester", "Mr. Poopybutthole", "Ghost in a Jar"],
+    correctAnswer: "Mr. Poopybutthole",
+    image:"assets/images/hat.gif"
 }];
+
+// Next I am creating the functions
+
+var game = {
+    questions:questions,
+    currentQuestion: 0,
+    counter:countStartNumber,
+    correct:0,
+    incorrect: 0,
+
+    countdown: function (){
+
+        game.counter--;
+        $("#counter-number").html(game.counter);
+
+        if (game.counter === 0){
+
+            console.log("TIME UP");
+            game.timeUp();
+
+        }
+    },
+    
+    loadQuestion: function(){
+        time = setInterval(game.countdown, 1000);
+        panel.html("<h2>" + questions[this.currentQuestion].question + "</h2>" );
+        //creating a for loop for the questions
+        for (var i=0; i<questions[this.currentQuestion].answers.length; i++){
+            panel.append("<button class=answer-button" id="button" + "'data-name='" + questions[this.currentQuestion].answers[i] + "">"" + questions[this.currentQuestion].answers[i] + "</button>");
+        }
+    },
+
+    nextQuestion: function(){
+        game.counter = countStartNumber;
+        $("#counter-number").html(game.counter);
+        game.currentQuestion++;
+        game.loadQuestion();
+    },
+
+    TimeUp: function(){
+        clearInterval(timer);
+        $("#counter-number").html(game.counter);
+    }
+
+}
